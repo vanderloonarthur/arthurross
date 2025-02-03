36acde61b56be5ac541521df277f070a2af83ebe
vanderloonarthur <vanderloonarthur@gmail.com>
2025-02-03 10:31:18 +0100

    commit to server

--- AI/server.js
+++ AI/server.js
@@ -2,21 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-// Example 1: Allow all domains (not recommended for production)
-app.use(cors());
-
-// Example 2: Allow only specific domains
-const allowedOrigins = ['https://www.arthurross.nl'];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'), false);
-    }
-  }
-}));
+const cors = require('cors');
+app.use(cors()); // Allow all origins
 
 // Example route
 app.get('/api/data', (req, res) => {
@@ -26,3 +13,13 @@ app.get('/api/data', (req, res) => {
 app.listen(8080, () => {
   console.log('Server is running on http://127.0.0.1:8080');
 });
+
+app.use(cors({
+origin: (origin, callback) => {
+  if (allowedOrigins.indexOf(origin) !== -1) {
+    callback(null, true);
+  } else {
+    callback(new Error('Not allowed by CORS'), false);
+  }
+}
+}));
